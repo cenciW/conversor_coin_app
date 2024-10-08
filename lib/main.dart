b9ca280f9e68c4fcc33395770a4309e987f2ee73
Jose Augusto Cenci Castilho <ze.ccenci@gmail.com>
2024-10-08 14:27:27 +0100

    starting interface

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,75 @@ const requestUrl =
     'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL';
 
 Future<void> main() async {
+  //transformando em mapa
+  // print(json.decode(response.body)["USDBRL"]['bid']);
+  // print(json.decode(response.body)["EURBRL"]);
+  // print(json.decode(response.body)["BTCBRL"]);
+
+  print(await getData());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+Future<Map> getData() async {
+  //dado do fututro
   http.Response response = await http.get(Uri.parse(requestUrl));
 
-  //transformando em mapa
-  print(json.decode(response.body)["USDBRL"]['bid']);
-  print(json.decode(response.body)["EURBRL"]);
-  print(json.decode(response.body)["BTCBRL"]);
+  //retornando o json
+  return json.decode(response.body);
+}
 
-  runApp(MaterialApp(
-    home: Container(),
-  ));
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+          "\$ Conversor de Moedas \$",
+        ),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao carregar dados :(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.green,
+                );
+              }
+          }
+        },
+      ),
+    );
+  }
 }
